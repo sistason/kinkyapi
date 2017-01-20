1d558b53ac7cc101921c7d09a6d8a3b2829688a3
Kai Sisterhenn <sistason@sistason.de>
2017-01-20 19:34:47 +0100

    Release 1.0 Implemented remaining API functions Added indexes for kinkcom and kinkyapi Added css, bootstrap and html-templates /FIN

--- kinkcom/urls.py
+++ kinkcom/urls.py
@@ -2,13 +2,19 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^shoot/(?P<shootid>\d*|latest)$', views.shoot, name='shoot'),
-    url(r'^shoot_title/(?P<title>.+)$', views.shoot, name='shoot'),
+    url(r'^$', views.index, name='index'),
+
+    url(r'^shoot/(?P<shootid>\d*)$', views.shoot, name='shoot'),
+    url(r'(?s)^shoot_title/(?P<title>.+)$', views.shoot, name='shoot'),
     url(r'^shoot_date/(?P<date>.+)$', views.shoot, name='shoot'),
     url(r'^shoot_performer/(?P<performer_number>\d+)$', views.shoot, name='shoot'),
-    url(r'^shoot_performer_name/(?P<performer_name>.+)$', views.shoot, name='shoot'),
+    url(r'(?s)^shoot_performer_name/(?P<performer_name>.+)$', views.shoot, name='shoot'),
 
     url(r'^performer/(?P<performer_number>\d+)$', views.performer, name='performer'),
-    url(r'^performer_name/(?P<performer_name>.+)$', views.performer, name='performer'),
+    url(r'(?s)^performer_name/(?P<performer_name>.+)$', views.performer, name='performer'),
 
+    url(r'^site/(?P<name>.*)$', views.site, name='site'),
+    url(r'^site_main/(?P<name_main>.*)$', views.site, name='site'),
+
+    url(r'^dump/?$', views.dump_database, name='dump_database'),
 ]
